b248f31e5a7f0ea8607102c14d2fe11c1fa68359
Aone <quanyi.zhao@ucsf.edu>
2017-11-03 22:50:48 -0700

    Nov 3

--- ChIPseq.sh
+++ ChIPseq.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #####################################
-# Usage:                            #
-# Manual:                           #
+# Usage:	$1=reads1/file/to/path	#
+#			$2=reads2/file/to/path	#
+#			$3=output_file_name		#
 #####################################
 
 # path to reads
@@ -15,7 +16,7 @@ fastqc -f fastq $1
 fastqc -f fastq $2
 
 # cutadapt to trim adaptors
-cutadapt -f fastq -m 30 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o $3_R1_trimmed.gz -p $3_R2_trimmed.gz $1 $2
+cutadapt -f fastq -m 30 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o $3_R1_trimmed.gz -p $3_R2_trimmed.gz $1 $2 > $3_cutadapt.log
 
 # bwa mem alignment
 bwa mem -M -t 16 $bwaindex_hg19 $3_R1_trimmed.gz $3_R2_trimmed.gz > $3.sam 
@@ -35,7 +36,7 @@ samtools rmdup $3_srt.bam $3_rm.bam >> alignment.log
 bamToBed -i $3_rm.bam > $3.bed
 
 # peak calling macs2.0.9
-# macs2 -t ChIP.bam -c input.bam -f bam -g hs -n NAME
+# macs2 -t ChIP.bam -c input.bam -f bam -g hs -n $3 
 
 
 ################ END ################
