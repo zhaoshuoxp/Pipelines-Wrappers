b4c41de3404a59bb44baf8ac77bc019d4afaa19a
Aone <quanyiz@stanford.edu>
2018-08-29 14:06:41 -0700

    Aug 29

--- ATAC.sh
+++ ATAC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #####################################
-# Usage:	$1=reads1/file/to/path	#
-#			$2=reads2/file/to/path	#
-#			$3=output_prefix			#
+# Usage:    $1=reads1/file/to/path  #
+#           $2=reads2/file/to/path  #
+#           $3=output_prefix			#
 #####################################
 
 # check programs: 
@@ -19,6 +19,7 @@ which bedClip &>/dev/null || { echo "bedClip not found!"; exit 1; }
 READS1=$1
 READS2=$2
 NAME=$3
+threads=6
 wget https://github.com/broadinstitute/picard/releases/download/2.18.12/picard.jar
 curl -s ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/chromInfo.txt.gz | gunzip -c > hg19_len
 
@@ -44,16 +45,16 @@ fastqc -f fastq -o fastqc $2
 # cutadapt to trim adaptors
 cutadapt -f fastq -m 30 -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT -g AGATGTGTATAAGAGACAG -G AGATGTGTATAAGAGACAG -o $3_R1_trimmed.gz -p $3_R2_trimmed.gz $1 $2 > ./logs/$3_cutadapt.log
 
-# bowtie2 aligment #up to: 4 aligment/insert 2000bp
-bowtie2 -k 2 -X 2000 --local --mm -p 6 -x $bowtie2index_hg19 -1 $3_R1_trimmed.gz -2 $3_R2_trimmed.gz -S $3.sam
+# bowtie2 aligment #up to: 2 aligment/insert 2000bp
+bowtie2 -k 2 -X 2000 --local --mm -p $threads -x $bowtie2index_hg19 -1 $3_R1_trimmed.gz -2 $3_R2_trimmed.gz -S $3.sam
 
 echo 'Bowtie2 mapping summary:' > ./logs/$3_align.log
 tail -n 15 nohup.out >> ./logs/$3_align.log
 
 # samtools to BAM/sort
-samtools view -b -@ 6 $3.sam -o $3.bam
+samtools view -b -@ $threads $3.sam -o $3.bam
 
-samtools sort -@ 6 $3.bam -o $3_srt.bam
+samtools sort -@ $threads $3.bam -o $3_srt.bam
 
 # picard mark duplaicates
 java -jar picard.jar MarkDuplicates INPUT=$3_srt.bam OUTPUT=$3_mkdup.bam METRICS_FILE=./logs/$3_dup.log REMOVE_DUPLICATES=false
@@ -64,11 +65,11 @@ samtools flagstat $3_mkdup.bam >> ./logs/$3_align.log
 samtools index $3_mkdup.bam 
 
 # remove chrM reads
-samtools idxstats $3_mkdup.bam | cut -f 1 |grep -v M | xargs samtools view -b -@ 6 -o $3_chrM.bam $3_mkdup.bam
+samtools idxstats $3_mkdup.bam | cut -f 1 |grep -v M | xargs samtools view -b -@ $threads -o $3_chrM.bam $3_mkdup.bam
 samtools index $3_chrM.bam
 
 # remove unpaired/unmapped/duplicates/not primary alignments
-samtools view -f 2 -F 1804 -b -@ 6 -o $3_filtered.bam $3_chrM.bam
+samtools view -f 2 -F 1804 -b -@ $threads -o $3_filtered.bam $3_chrM.bam
 
 echo >> ./logs/$3_align.log
 echo 'flagstat after filter:' >> ./logs/$3_align.log
@@ -84,14 +85,21 @@ cat $3_se.bed | sort -k1,1 | bedItemOverlapCount hg19 -chromSize=hg19_len stdin 
 bedGraphToBigWig $3.bedGraph hg19_len $3_bam.bw
 
 # Tn5 shifting
-samtools sort -n -@ 6 -o $3_pe.bam $3_filtered.bam
+samtools sort -n -@ $threads -o $3_pe.bam $3_filtered.bam
 bamToBed -bedpe -i $3_pe.bam > $3_pe.bed
 
 awk -v OFS="\t" '{if($9=="+"){print $1,$2+4,$6+4}else if($9=="-"){if($2>=5){print $1,$2-5,$6-5}else if($6>5){print $1,0,$6-5}}}' $3_pe.bed > $3_shift.bed
 
+### SE MODE ###
+#awk -F $'\t' 'BEGIN{OFS=FS}{if($6=="+"){$2=$2+4}else if($6=="-"){$3=$3-5} print $0}' $3_se.bed > $3_shift_se.bed
+
+
 # macs2 call broad peaks
 cd macs2
 macs2 callpeak -t ../$3_shift.bed -g hs -n $3 -f BEDPE --keep-dup all --broad -B --SPMR 
+
+### SE MODE ###
+#macs2 callpeak -t ../$3_shift_se.bed -g hs -n $3 -f BED --keep-dup all --broad -B --SPMR --nomodel --shift -37 --extsize 73
 
 mv $3_control_lambda.bdg $3_control_lambda_SPMR.bdg
 mv $3_treat_pileup.bdg $3_treat_pileup_SPMR.bdg
@@ -105,6 +113,9 @@ rm bedGraph2bigwig.sh
 
 # macs2 call narrow peaks
 macs2 callpeak -t ../$3_shift.bed -g hs -n $3 -f BEDPE -B --keep-dup all
+
+### SE MODE ###
+#macs2 callpeak -t ../$3_shift_se.bed -g hs -n $3 -f BED --keep-dup all -B --nomodel --shift -37 --extsize 73
 
 mv $3_peaks.xls $3_narrow.xls
 
