21ecdd0185a26998535590796119c5a86e50596a
Aone <quanyiz@stanford.edu>
2019-02-14 12:58:57 -0800

    rm dup randomly

---
+++ rmdup_rdm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#####################################
+# Usage:                            #
+# Manual:                           #
+#####################################
+
+java -jar /home/quanyi/app/picard.jar MarkDuplicates INPUT=$1 OUTPUT=$2 METRICS_FILE=$(basename $2 .bam)_dup.log REMOVE_DUPLICATES=true DUPLICATE_SCORING_STRATEGY=RANDOM
+
+################ END ################
+#          Created by Aone          #
+#     quanyi.zhao@stanford.edu      #
+################ END ################
