9740e40b427ef76097c318a9be21b4d8e471acfd
Aone <quanyiz@stanford.edu>
2019-03-01 12:08:24 -0800

    cutadapt wrapper

---
+++ adapt_trim.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#####################################
+# Usage:                            #
+# Manual:                           #
+#####################################
+# multi-core support requires cutadapt installed and run by python3
+which cutadapt &>/dev/null || { echo "cutadapt not found, install by pip3!"; exit 1; }
+which python3 &>/dev/null || { echo "python3 not found, install python3!"; exit 1; }
+
+# help message
+help(){
+	cat <<-EOF
+	adapt_trim.sh <options> <reads1>|..<reads2> 
+	-t Threads (1 default)
+	-s Single-end mod (Paired-end default)
+	-n Nextera adapters (Trueseq default)
+	-h Print this help message
+EOF
+	exit 0
+}
+
+# cutadapt CMD
+adapt_trim(){
+	if [ $1='se' ];then
+		# single-end CMD
+		cutadapt -m 30 -j $2 -a $3 -g $4 -o ${5}_trimmed.fastq.gz $6 > ${5}_cutadapt.log
+	else
+		# paired-end CMD
+		cutadapt -m 30 -j $2 -a $3 -A $3 -g $4 -G $4 -o ${5}_trimmed_R1.fastq.gz -p ${5}_trimmed_R2.fastq.gz $6 $7 > ${5}_cutadapt.log
+	fi
+}
+
+# no ARGs error
+if [ $# -lt 1 ]
+then
+	help
+	exit 1
+fi
+
+# default Paired-end mod
+mod='pe'
+# default TrueSeq adapters
+aA='AGATCGGAAGAGC'
+gG='GCTCTTCCGATCT'
+# default 1 core to run
+threads=1
+
+while getopts "snt:h" arg
+do
+	case $arg in
+		t) threads=$OPTARG;;
+		# single-end mod
+		s) mod='se';;
+		# Nextera adapters
+		n) aA='CTGTCTCTTATACACATCT'
+		   gG='AGATGTGTATAAGAGACAG';;
+		h) help ;;
+		?) help
+			exit 1;;
+	esac
+done
+
+# shift ARGs to reads
+shift $(($OPTIND - 1))
+# get prefix of output
+prefix=${1%.*}
+
+# main
+adapt_trim $mod $threads $aA $gG $prefix $1 $2
+
+# check running status
+if [ $? -ne 0 ]; then
+	help
+	exit 1
+else
+	echo "Run succeed"
+fi
+
+################ END ################
+#          Created by Aone          #
+#     quanyi.zhao@stanford.edu      #
+################ END ################
