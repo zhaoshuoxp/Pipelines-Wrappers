8a46c91aac49541bca2a74bfb162afce225a6fa4
Aone <quanyiz@stanford.edu>
2019-03-11 12:38:05 -0700

    Update rmdup_rdm.sh

--- rmdup_rdm.sh
+++ rmdup_rdm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #####################################
-# Usage:                            #
-# Manual:                           #
+# Usage: rmdup_rdm.sh <in bam> <out bam>
 #####################################
-
+echo "remove duplicates RANDOMLY with picard"
 java -jar /home/quanyi/app/picard.jar MarkDuplicates INPUT=$1 OUTPUT=$2 METRICS_FILE=$(basename $2 .bam)_dup.log REMOVE_DUPLICATES=true DUPLICATE_SCORING_STRATEGY=RANDOM
 
 ################ END ################
