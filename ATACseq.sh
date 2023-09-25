60656c3039232812bcb6b28bae67c95bbecae1d9
Quanyi Zhao <quanyiz@stanford.edu>
2023-09-25 16:23:36 -0700

    Update ATACseq.sh

--- ATACseq.sh
+++ ATACseq.sh
@@ -129,7 +129,7 @@ sam_bam_bed(){
 		samtools sort -n -@ $threads -o ${1}.bam2 ${1}_filtered.bam
 		# bam2bedpe
 		bamToBed -bedpe -i ${1}.bam2 > ${1}.bedpe
-		bamToBed -i ${1}_filtered.bam > ${1}_se.bed
+		bamToBed -i ${1}_filtered.bam > ${1}.bed
 		# clean
 		rm ${1}_srt.bam ${1}.bam2 picard.jar 
 	fi
@@ -142,7 +142,7 @@ sam_bam_bed(){
 peak_calling(){
 	if [ $1 = 'se' ];then
 		# Tn5 shift in SE mode
-		awk -F $'\t' 'BEGIN{OFS=FS}{if($6=="+"){$2=$2+4}else if($6=="-"){${3}=${3}-5} print $0}' ${2}_se.bed > ${2}_shift_se.bed
+		awk -F $'\t' 'BEGIN{OFS=FS}{if($6=="+"){$2=$2+4}else if($6=="-"){$3=$3-5} print $0}' ${2}_se.bed > ${2}_shift_se.bed
 		# broad peak calling
 		cd macs2
 		macs2 callpeak -t ../${2}_shift_se.bed -g $sp -n ${2} -f BED --keep-dup all --broad --nomodel --shift -37 --extsize 73
@@ -150,16 +150,16 @@ peak_calling(){
 		intersectBed -v -a ${2}_peaks.broadPeak -b $blkt_file > ${2}_broad_filtered.bed
 	else
 		# Tn5 shift in PE mode
-		awk -v OFS="\t" '{if($9=="+"){print $1,$2+4,$6+4}else if($9=="-"){if($2>=5){print $1,$2-5,$6-5}else if($6>5){print $1,0,$6-5}}}' ${2}.bedpe > ${2}_shift_pe.bed
-		awk -F $'\t' 'BEGIN{OFS=FS}{if($6=="+"){$2=$2+4}else if($6=="-"){${3}=${3}-5} print $0}' ${2}_se.bed > ${2}_shift_se.bed
+		awk -v OFS="\t" '{if($9=="+"){print $1,$2+4,$6+4}else if($9=="-"){if($2>=5){print $1,$2-5,$6-5}else if($6>5){print $1,0,$6-5}}}' ${2}.bedpe > ${2}_pe.bed
+		awk -F $'\t' 'BEGIN{OFS=FS}{if($6=="+"){$2=$2+4}else if($6=="-"){$3=$3-5} print $0}' ${2}.bed > ${2}_se.bed
 		# broad peak calling
 		cd macs2
 		echo "MACS2 version >= 2.1.1 required!"
-		macs2 callpeak -t ../${2}_shift_pe.bed -g $sp -n ${2} -f BEDPE --keep-dup all --broad 
+		macs2 callpeak -t ../${2}_pe.bed -g $sp -n ${2} -f BEDPE --keep-dup all --broad 
 		# Blacklist filter 
 		intersectBed -v -a ${2}_peaks.broadPeak -b $blkt_file > ${2}_broad_filtered.bed
 	fi
-	rm  ${2}_se.bed ${2}.bedpe
+	rm ${2}.bedpe ${2}.bed 
 }
 
 chromap_total(){
@@ -174,7 +174,7 @@ chromap_total(){
 		tail -n 14 nohup.out >> ./logs/${1}_align.log
 		awk 'substr($1,1,3)=="chr"' ${1}_pe.bed > ${1}_pri.bed
 		len=$(gunzip -c $3 |head -n2|tail -n1|awk '{print length($0)}')
-		awk -v l=$len -v OFS="\t" '{print $1,$2,$2+l,$4,$5,$6"\n"$1,$3-l,$3,$5,$6}'  ${1}_pri.bed > ${1}_se.bed
+		awk -v l=$len -v OFS="\t" '{print $1,$2,$2+l,$4,$5,$6"\n"$1,$3-l,$3,$4,$5,$6}'  ${1}_pri.bed > ${1}_se.bed
 	fi
 	factor=$(wc -l ${1}_pri.bed|awk '{print 1000000/$1}')
 	genomeCoverageBed -scale $factor -i ${1}_pri.bed -g $chromsize -bg > ${1}.bdg
@@ -185,9 +185,11 @@ chromap_total(){
 	cd macs2
 	echo "MACS2 version >= 2.1.1 required!"
 	if [ $2 = 'se' ];then
-		macs2 callpeak -t ../${1}_pri.bed -g $sp -n ${1} -f BED --keep-dup all --broad
+		mv ${1}_pri.bed ${1}_se.bed
+		macs2 callpeak -t ../${1}_se.bed -g $sp -n ${1} -f BED --keep-dup all --broad
 	else
-		macs2 callpeak -t ../${1}_pri.bed -g $sp -n ${1} -f BEDPE --keep-dup all --broad 
+		mv ${1}_pri.bed ${1}_pe.bed 
+		macs2 callpeak -t ../${1}_pe.bed -g $sp -n ${1} -f BEDPE --keep-dup all --broad 
 	fi
 	# Blacklist filter 
 	intersectBed -v -a ${1}_peaks.broadPeak -b $blkt_file > ${1}_broad_filtered.bed
